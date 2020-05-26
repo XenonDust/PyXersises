52b91996bf6e01cd3e2da5123a3cdb8e32590254
Charan Sritej <charansritej@gmail.com>
2020-05-26 21:31:59 +1000

    new function

--- ex1.py
+++ ex1.py
@@ -1,9 +1,20 @@
 
 
-ret_list=[]
+
 def new_ticket(airline,srce,dstn,counter):
     print("AL1:"+(srce[:3].upper())+":"+(dstn[:3].upper())+":"+str(counter)
     ret_list.append(counter+1)
     if len(ret_list)>5:
         ret_list.remove(ret_list[0])
     return ret_list
+
+
+def proper_divisors(num):
+    ret_list = []
+    for i in range(1,(int(num/2))+1):
+        if num%i == 0:
+            ret_list.append(i)
+    return ret_list[]
+
+def generate_fibonacci(num):
+    
